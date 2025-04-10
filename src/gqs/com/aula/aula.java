6f7e1a686abdaed2b674d59e42d04485679cfd1d
Augusto Assis <augustoassis.dev@gmail.com>
2025-04-10 20:54:31 -0300

    fix: faltou o ponto e virgula e adicionando 'From Java' na mensagem de hello world

--- src/gqs/com/aula/aula.java
+++ src/gqs/com/aula/aula.java
@@ -1,6 +1,7 @@
-package src.gqs.com.aula
+package src.gqs.com.aula;
+
 public class aula{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World from Java!");
     }
 }
