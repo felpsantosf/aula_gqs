c0f8d775daef41f9c0d4bd4b7e22a5522642f365
felpsantosf <90201707+felpsantosf@users.noreply.github.com>
2025-04-22 20:16:43 -0300

    Adcionado a classe Cliente

--- src/gqs/com/aula/aula.java
+++ src/gqs/com/aula/aula.java
@@ -1,7 +1,38 @@
-package src.gqs.com.aula;
+public class Cliente {
+    private int id;
+    private String nome;
+    private int desconto;
+    private String genero;
 
-public class aula{
-    public static void main(String[] args) {
-        System.out.println("Hello World from Java!");
+    public Cliente(int id, String nome, int desconto) {
+        this.id = id;
+        this.nome = nome;
+        this.desconto = desconto;
+        this.genero = "nd";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+
+    
+    public String toString() {
+        return nome + "(" + id + ")(" + desconto + "%)";
     }
 }
